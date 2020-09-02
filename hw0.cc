2e641bd5118d8b8d77235130308c7460ca128568
SunnyCheng1125 <32073042+SunnyCheng1125@users.noreply.github.com>
2020-09-02 11:17:12 -0600

    Add files via upload

---
+++ hw0.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // How many CU students does it take to change a light bulb?
+    cout << "Ten — nine to stare at light bulb, and only one to google how to change a light bulb\n";
+    return 0;
+}
